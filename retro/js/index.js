0d35aa66875bc6e02dd7f71d55c285981dc3405c
Shubhanshu Kumar <103992054+shubhanshussm@users.noreply.github.com>
2023-12-20 16:26:05 +0530

    Add files via upload

---
+++ retro/js/index.js
@@ -0,0 +1,14 @@
+document.querySelector(".icon").onclick= ()=>{
+    // console.log("hii");
+    document.querySelector("ul").style.left = 0
+    document.querySelector(".fa-close").classList.add("active")
+    document.querySelector(".fa-close").style.display = "block"
+   
+}
+
+document.querySelector(".fa-close").onclick= ()=>{
+    // console.log("hello");
+    document.querySelector("ul").style.left = "100%"
+    document.querySelector(".fa-close").classList.remove("active")
+    document.querySelector(".fa-close").style.display = "none"
+}
